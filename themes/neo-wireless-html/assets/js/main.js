2a75af8f660d6d1f9d1e90198abd315c8a5fc951
Rannojit <cserannojitsarkar22@gmail.com>
2019-10-24 22:26:29 +0600

    banner plugin bubble size update

--- themes/neo-wireless-html/assets/js/main.js
+++ themes/neo-wireless-html/assets/js/main.js
@@ -635,7 +635,7 @@ if( $('#particles-js').length ){
       }
     },
     "size": {
-      "value": 3,
+      "value": 7,
       "random": true,
       "anim": {
         "enable": false,
@@ -711,23 +711,7 @@ if( $('#particles-js').length ){
 
     /* ---- stats.js config ---- */
 
-    var count_particles, stats, update;
-    stats = new Stats;
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-    count_particles = document.querySelector('.js-count-particles');
-    update = function() {
-      stats.begin();
-      stats.end();
-      if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-      }
-      requestAnimationFrame(update);
-    };
-    requestAnimationFrame(update);
+
 }
 
     new WOW().init();
