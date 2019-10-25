e492c5eea82766978fe201a654f706eb5fcb3eb0
azizulsoftsw <azizul.softsw@gmail.com>
2019-10-25 11:13:58 +0600

    settings

---
+++ themes/neo-wireless-html/footer.php
@@ -0,0 +1,242 @@
+<footer class="footer-wrap">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+          <div class="ftr-top-innr clearfix">
+            <div class="ftr-lft-logo clearfix">
+              <div class="ftr-logo">
+                <a href="#">
+                  <img src="<?php echo THEME_URI; ?>/assets/images/ftr-logo.png">
+                </a>
+              </div>
+              <div class="ftr-mid-socail show-xs">
+                 <a href="#">
+                    <i>
+                      <svg class="ftr-fb-icon" width="14" height="24" viewBox="0 0 14 24" fill="#43587B;">
+                        <use xlink:href="#ftr-fb-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+                  <a href="#">
+                    <i>
+                      <svg class="ftr-twi-icon" width="24" height="22" viewBox="0 0 24 22" fill="#43587B;">
+                        <use xlink:href="#ftr-twi-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+                  <a href="#">
+                    <i>
+                      <svg class="ftr-ins-icon" width="24" height="24" viewBox="0 0 24 24" fill="#43587B;">
+                        <use xlink:href="#ftr-ins-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+              </div>
+              <div class="ftr-tp-info">
+                <a href="#">Burgemeester De Cocklaan 10 <br> bus 11 9320 Erembodegem</a>
+                <a href="mailto:info@neowireless.eu">info@neowireless.eu</a>
+                <span>BE0848 529 769</span>
+              </div>
+              <div class="ftr-mid-innr show-xs clearfix">
+                <div class="ftr-mid-contact">
+                  <a href="tel:+32 53 86 00 88">
+                    <i>
+                      <svg class="ftr-phone-icon" width="38" height="38" viewBox="0 0 38 38" fill="#43587B;">
+                        <use xlink:href="#ftr-phone-icon-svg"></use>
+                      </svg> 
+                    </i>
+                   Call us: +32 53 86 00 88
+                  </a>
+                </div>
+                <div class="ftr-mid-socail hide-xs">
+                  <a href="#">
+                    <i>
+                      <svg class="ftr-fb-icon" width="14" height="24" viewBox="0 0 14 24" fill="#43587B;">
+                        <use xlink:href="#ftr-fb-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+                  <a href="#">
+                    <i>
+                      <svg class="ftr-twi-icon" width="24" height="22" viewBox="0 0 24 22" fill="#43587B;">
+                        <use xlink:href="#ftr-twi-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+                  <a href="#">
+                    <i>
+                      <svg class="ftr-ins-icon" width="24" height="24" viewBox="0 0 24 24" fill="#43587B;">
+                        <use xlink:href="#ftr-ins-icon-svg"></use>
+                      </svg> 
+                    </i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="ftr-tp-menus">
+              <div class="footer-menu-itm">
+                <h5>Navigation</h5>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+              <div class="footer-menu-itm">
+                <h5>Services & Products</h5>
+                <ul>
+                  <li><a href="#">Mounting accesories</a></li>
+                  <li><a href="#">Wi-fi & Lan Projects</a></li>
+                  <li><a href="#">Realizations</a></li>
+                </ul>
+              </div>
+              <div class="footer-menu-itm">
+                <h5>Product Categories</h5>
+                <ul>
+                  <li><a href="#">Product Categories</a></li>
+                  <li><a href="#">Product Category</a></li>
+                  <li><a href="#">Product Category</a></li>
+                  <li><a href="#">Product Category</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-mid-innr hide-xs clearfix">
+          <div class="ftr-mid-contact">
+            <a href="tel:+32 53 86 00 88">
+              <i>
+                <svg class="ftr-phone-icon" width="38" height="38" viewBox="0 0 38 38" fill="#43587B;">
+                  <use xlink:href="#ftr-phone-icon-svg"></use>
+                </svg> 
+              </i>
+             Call us: +32 53 86 00 88
+            </a>
+          </div>
+          <div class="ftr-mid-socail hide-xs">
+            <a href="#">
+              <i>
+                <svg class="ftr-fb-icon" width="14" height="24" viewBox="0 0 14 24" fill="#43587B;">
+                  <use xlink:href="#ftr-fb-icon-svg"></use>
+                </svg> 
+              </i>
+            </a>
+            <a href="#">
+              <i>
+                <svg class="ftr-twi-icon" width="24" height="22" viewBox="0 0 24 22" fill="#43587B;">
+                  <use xlink:href="#ftr-twi-icon-svg"></use>
+                </svg> 
+              </i>
+            </a>
+            <a href="#">
+              <i>
+                <svg class="ftr-ins-icon" width="24" height="24" viewBox="0 0 24 24" fill="#43587B;">
+                  <use xlink:href="#ftr-ins-icon-svg"></use>
+                </svg> 
+              </i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-btm-innr clearfix">
+          <div class="ftr-btm-menu">
+            <p>&copy; 2019 Neo Wireless. All Rights Reserved.</p>
+            <ul class="clearfix">
+              <li><a href="#">Sitemap</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Cookie Policy</a></li>
+            </ul>
+          </div>
+          <div class="ftr-btm-rgt-con">
+            <a href="#">webdesign by conversal</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+</footer><!--end of footer-wrap -->
+
+<div class="home-bnr-xs-nav-bar-controller show-xs">
+  <div class="xs-menu-btn-bar clearfix">
+      <div class="xs-menu-btn-contact">
+        <a href="#">
+          <i><img src="<?php echo THEME_URI; ?>/assets/images/cart-xs-icon.svg"></i>
+        </a>
+      </div>
+      <div class="nav-opener">
+       <div class="nav-opener-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <strong>MENU</strong>
+     </div>
+  </div>
+</div>
+
+<div class="xs-popup-main-menu-controller">
+    <div class="xs-popup-logo">
+      <a href="#"><img src="<?php echo THEME_URI; ?>/assets/images/logo.svg"></a>
+    </div>
+    <div class="hdr-btm-nav">
+      <ul class="clearfix ulc">
+        <li><a href="#">products</a></li>
+        <li><a href="#">Wi-Fi / LAN</a></li>
+      </ul>
+    </div>
+    <nav class="xs-popup-main-nav clearfix">
+      <ul class="clearfix ulc">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us </a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="hdr-search">
+      <form>
+        <input type="search" name="" placeholder="Search">
+        <button>
+          <em><img src="<?php echo THEME_URI; ?>/assets/images/search-icon.svg"></em>
+        </button>
+      </form>
+    </div>
+    <div class="hdr-cart-btn">
+      <em><img src="<?php echo THEME_URI; ?>/assets/images/cart-icon-white.svg">Shopping Cart</em>
+    </div>
+    <div class="nw-lang">
+      <ul class="ulc">
+        <li class="lag-active"><a href="#">EN </a></li>
+        <li><a href="#">ES</a></li>
+      </ul>
+    </div>
+    <div class="xs-menu-btn-bar-popup">
+      <div class="xs-menu-btn-bar clearfix">
+        <div class="xs-menu-btn-contact">
+          <a href="#">
+            <i><img src="<?php echo THEME_URI; ?>/assets/images/cart-xs-icon.svg"></i>
+          </a>
+        </div>
+        <div class="nav-opener">
+         <div class="nav-opener-btn">
+            <img src="<?php echo THEME_URI; ?>/assets/images/close-icon.svg">
+          </div>
+          <strong>CLOSE</strong>
+       </div>
+      </div>
+    </div>
+</div>
+
+<script src="assets/js/jquery.sticky-sidebar.js"></script>
+<script src="assets/js/jquery.matchHeight-min.js"></script>
+<script src="assets/js/particles.min.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
