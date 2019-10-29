cc5d534f94bf5b4c2ffa5bcb938d6a3c5c28c4cc
azizulsoftsw <azizul.softsw@gmail.com>
2019-10-30 01:35:58 +0600

    sidebar add

--- themes/neo-wireless-html/inc/widgets-area.php
+++ themes/neo-wireless-html/inc/widgets-area.php
@@ -2,22 +2,13 @@
 
 function deploy_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar widget one', 'demula' ),
-		'id'            => 'sidebar-widget-one',
-		'description'   => __( 'Add widgets here to appear in your job single page.', 'demula' ),
+		'name'          => __( 'Shop Sidebar Widget', 'demula' ),
+		'id'            => 'shop-widget',
+		'description'   => __( 'Add widgets here to appear in your shop page.', 'demula' ),
 		'before_widget' => '',
 		'after_widget'  => '',
 		'before_title'  => '<h3>',
 		'after_title'   => '</h3>',
-	) );
-	register_sidebar( array(
-		'name'          => __( 'Footer Navigation', 'demula' ),
-		'id'            => 'footer-widget-nav',
-		'description'   => __( 'Add widgets here to appear in your footer navigation.', 'demula' ),
-		'before_widget' => '',
-		'after_widget'  => '',
-		'before_title'  => '<h6>',
-		'after_title'   => '</h6>',
 	) );
 }
 add_action( 'widgets_init', 'deploy_widgets_init' );
