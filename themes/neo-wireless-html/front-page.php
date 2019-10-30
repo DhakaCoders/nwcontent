0360adcb21969bc8a085121932a053d47a80c76d
Rannojit <cserannojitsarkar22@gmail.com>
2019-10-30 20:59:06 +0600

    home banner fix

--- themes/neo-wireless-html/front-page.php
+++ themes/neo-wireless-html/front-page.php
@@ -39,12 +39,14 @@ if($banner):
             <?php foreach($posters as $poster): $link3 = $poster['link'];?>
               <div class="main-bnr-img-bx-scale">
                 <a class="overlay-link" href="<?php echo $link3['url']; ?>"></a>
-                <div class="main-bnr-img-bx" style="background: url(<?php echo $poster['image']; ?>);">
+                <div>
+                  <div class="main-bnr-img-bx" style="background: url(<?php echo $poster['image']; ?>);">
+                  </div>
                   <?php 
-                    if( is_array( $link3 ) &&  !empty( $link3['url'] ) ){
-                      printf('<a href="%s" target="%s">%s<em><img src="'.THEME_URI.'/assets/images/white-link-arrow.svg"></em></a>', $link3['url'], $link3['target'], $link3['title']); 
-                    }
-                  ?>
+                      if( is_array( $link3 ) &&  !empty( $link3['url'] ) ){
+                        printf('<a href="%s" target="%s">%s<em><img src="'.THEME_URI.'/assets/images/white-link-arrow.svg"></em></a>', $link3['url'], $link3['target'], $link3['title']); 
+                      }
+                    ?>
                 </div>
               </div>
             <?php endforeach; ?>
@@ -75,12 +77,17 @@ if($banner):
       <div class="col-sm-12">
         <div class="main-bnr-box-imgs">
           <?php foreach($posters as $poster): $link3 = $poster['link'];?>
-          <div class="main-bnr-img-bx" style="background: url(<?php echo $poster['image']; ?>);">
-            <?php 
-              if( is_array( $link3 ) &&  !empty( $link3['url'] ) ){
-                printf('<a href="%s" target="%s">%s<em><img src="'.THEME_URI.'/assets/images/white-link-arrow.svg"></em></a>', $link3['url'], $link3['target'], $link3['title']); 
-              }
-            ?>
+          <div class="main-bnr-img-bx-scale">
+            <a class="overlay-link" href="<?php echo $link3['url']; ?>"></a>
+            <div>
+              <div class="main-bnr-img-bx" style="background: url(<?php echo $poster['image']; ?>);">
+                </div>
+                <?php 
+                    if( is_array( $link3 ) &&  !empty( $link3['url'] ) ){
+                      printf('<a href="%s" target="%s">%s<em><img src="'.THEME_URI.'/assets/images/white-link-arrow.svg"></em></a>', $link3['url'], $link3['target'], $link3['title']); 
+                    }
+                  ?>
+            </div>
           </div>
           <?php endforeach; ?>
         </div>
